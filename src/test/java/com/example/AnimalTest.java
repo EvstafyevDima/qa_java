f9c5115231572a6044f42f33ca79e6f24f3c0011
Evstafev Dima <evstafyevdi@gmail.com>
2022-03-05 15:21:02 +0500

    Сделал проект

---
+++ src/test/java/com/example/AnimalTest.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AnimalTest {
+
+    @Test
+    public void methodGetFamilyShouldReturnStr() {
+        Animal animal = new Animal();
+        String expected = "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
+        String actual = animal.getFamily();
+        Assert.assertEquals(expected, actual);
+    }
+
+}
